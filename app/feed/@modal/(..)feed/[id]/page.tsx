649f0f5ff8381329f04f16538dca93d5e4c5f9e1
abdullahh-dev <abdullahaha101@example.com>
2024-11-21 18:40:09 +0500

    added aysnc

--- app/feed/@modal/(..)feed/[id]/page.tsx
+++ app/feed/@modal/(..)feed/[id]/page.tsx
@@ -1,8 +1,8 @@
 import ImageCard from "@/components/ImageCard";
 import images from "@/public/images";
 
-const SingleFeedImagePage = ({ params }: { params: { id: number } }) => {
-  const imageId = params.id;
+const SingleFeedImagePage = async ({ params }: { params: { id: number } }) => {
+  const imageId = Number(params.id);
   const image = images.find(({ id }) => id == imageId);
 
   if (!image) return <div>Image not found</div>;
