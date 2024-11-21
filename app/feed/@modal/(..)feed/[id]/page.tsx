b7696a9b8f7fb3d3c92115cf14b6e0b3609a3322
abdullahh-dev <abdullahaha101@example.com>
2024-11-21 18:44:05 +0500

    fixed type issue

--- app/feed/@modal/(..)feed/[id]/page.tsx
+++ app/feed/@modal/(..)feed/[id]/page.tsx
@@ -1,8 +1,12 @@
 import ImageCard from "@/components/ImageCard";
 import images from "@/public/images";
 
-const SingleFeedImagePage = async ({ params }: { params: { id: number } }) => {
-  const imageId = Number(params.id);
+const SingleFeedImagePage = async ({
+  params,
+}: {
+  params: Promise<{ id: number }>;
+}) => {
+  const imageId = (await params).id;
   const image = images.find(({ id }) => id == imageId);
 
   if (!image) return <div>Image not found</div>;
